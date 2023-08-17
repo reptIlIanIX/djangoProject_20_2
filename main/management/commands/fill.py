8556832056326e2b4953a6aae2165deb7bac797b
Denis_b <spawnslash@mail.ru>
2023-08-17 14:22:45 +0300

    hw_20_1 done

--- main/management/commands/fill.py
+++ main/management/commands/fill.py
@@ -1,14 +1,21 @@
 from django.core.management import BaseCommand
 
-from main.models import Product
+from main.models import Product, Category
 
 
 class Command(BaseCommand):
     Product.objects.all().delete()
+    Category.objects.all().delete()
 
     def handle(self, *args, **options):
-        products_list = [{'name': 'orange', 'description': 'orange', 'category_id': 7, 'price': 13},
-                         {'name': 'apple', 'description': 'apple', 'category_id': 7, 'price': 21}]
+        categories_list = [{'id': 1, 'name': 'fruit', 'description': 'fruity'},
+                           {'id': 2, 'name': 'vegetable', 'description': 'vegetable'}]
+
+        products_list = [{'name': 'orange', 'description': 'orange', 'category_id': 1, 'price': 13},
+                         {'name': 'apple', 'description': 'apple', 'category_id': 1, 'price': 21}]
+
+        for item in categories_list:
+            Category.objects.create(**item)
 
         for item in products_list:
             Product.objects.create(**item)
