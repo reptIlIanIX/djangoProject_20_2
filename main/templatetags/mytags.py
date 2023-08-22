5b3e12da4b1ec9ce83f39272c7fcc9eaa10f060c
Denis_b <spawnslash@mail.ru>
2023-08-22 12:38:06 +0300

    hw_20_2 done

---
+++ main/templatetags/mytags.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def my_image(image):
+    if image:
+        return f'./media/{image}'
+    return ''
+
+
+@register.simple_tag
+def mediapath(image):
+    if image:
+        return f'./media/{image}'
+    return ''
