eea3424ce7e54dca6d5f8eb65c59346be3399c5d
Denis_b <spawnslash@mail.ru>
2023-08-23 18:15:57 +0300

    hw_20_3 read and view

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from main.apps import MainConfig
 from main.views import contacts, ProductListView, ProductDetailView
 
-app_name = 'main'
+app_name = MainConfig.name
 
 urlpatterns = [path('', ProductListView.as_view(), name='read'),
-               path('product/<int:pk>', ProductDetailView.as_view(), name='view_product')]
-
-# path('contacts/', contacts),
+               path('product/<int:pk>', ProductDetailView.as_view(), name='view_product'), path('contacts/', contacts)]
--- main/views.py
+++ main/views.py
@@ -17,7 +17,3 @@ def contacts(request):
     return render(request, 'main/contacts.html')
 
 
-# def product(request, pk: int):
-#     context = {'object_list': Product.objects.filter(id=pk)}
-#
-#     return render(request, 'main/product.html', context)
