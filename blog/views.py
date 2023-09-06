c33ff9b376169d9e679c2b96d70b66d0d02dbb54
Denis_b <spawnslash@mail.ru>
2023-09-06 16:50:58 +0300

    hw_22_4 start

--- blog/views.py
+++ blog/views.py
@@ -25,31 +25,28 @@ class BlogDetailView(DetailView):
         return self.object
 
 
-# class BlogCreateView(CreateView):
-#     model = Blog
-#     fields = ('topic', 'description', 'image', 'date_start', 'is_published')
-#     success_url = reverse_lazy('blog:view')
-#
-#     def form_valid(self, form):
-#         if form.is_valid():
-#             new_mat = form.save()
-#             new_mat.slug = slugify(new_mat.topic)
-#             new_mat.save()
-#         return super().form_valid(form)
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('topic', 'description', 'image', 'date_start', 'is_published')
+    success_url = reverse_lazy('blog:view')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.topic)
+            new_mat.save()
+        return super().form_valid(form)
 
 
-# class BlogUpdateView(UpdateView):
-#     model = Blog
-#     fields = ('topic', 'description', 'image', 'date_start', 'is_published')
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('topic', 'description', 'image', 'date_start', 'is_published')
 
     def get_success_url(self):
         return reverse('blog:view_blog', args=[self.object.id])
 
 
-# class BlogDeleteView(DeleteView):
-#     model = Blog
-#     form_class = BlogForm
-#     success_url = reverse_lazy('blog:view')
-
-class BlogCreateVIew(CreateView):
+class BlogDeleteView(DeleteView):
     model = Blog
+    success_url = reverse_lazy('blog:view')
+
--- djangoProject_20_2/urls.py
+++ djangoProject_20_2/urls.py
@@ -23,6 +23,6 @@ from django.urls import path, include
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('main.urls', namespace='main')),
-                  #      path('', include('blog.urls', namespace='blog'))
+                  path('blog/', include('blog.urls', namespace='blog'))
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
